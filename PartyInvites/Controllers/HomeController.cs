e99423926c7ab7b982258542272880876a2bd935
Keneil Jordan <keneiljordan@hotmail.co.uk>
2021-02-11 20:10:52 +0000

    Add project files.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //Stores Guest Response using HTTP POST if it is valid 
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        // Calls a View method using the repository. Razor renders a default view using the action method name.
+        public ViewResult ListResponses(GuestResponse guestResponse)
+        {
+            // LINQ filters the model data so that it returns all responses that will attend only.
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        // Initialise a new list for guest responses
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+        
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        //Method for adding a response to the GuestResponse list
+        public static void AddResponse(GuestResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
